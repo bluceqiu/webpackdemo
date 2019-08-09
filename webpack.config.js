0055c1e052958bb3112deb8075e13a2239bf1a38
xiaolong.qiu <xiaolong.qiu@dmall.com>
2019-08-09 21:10:56 +0800

    配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 // 基于node， 使用node的语法
+let path = require('path');
 module.exports = {
-    entry: '',
-    output: {},
-    devServer: {},
+    entry: './src/index.js', // 入口文件
+    output: {
+        filename: 'build.js',
+        path: path.resolve('./build')
+    },
+    devServer: {
+        contentBase: '/build',
+        port: 3000,
+        compress: true, // 服务器压缩
+        open: true // 自动打开浏览器
+    },
     module: {},
     plugins: [],
     mode: 'development',
