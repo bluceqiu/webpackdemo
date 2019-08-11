afae64ba59e6bdf5f9e663addd8dda756c5a9dd5
xiaolong.qiu <xiaolong.qiu@dmall.com>
2019-08-11 14:02:39 +0800

    webpack 基础构建

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('hello');
+// let a = require('./a');
+document.getElementById('app').innerHTML = 'ok';
+// document.write('<b>index.js111111</b>');
+import './index.css';
+// import './index.sass';
+// module.hot && module.hot.accept();
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,76 @@
 // 基于node， 使用node的语法
 let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let { CleanWebpackPlugin } = require('clean-webpack-plugin');
+let Webpack = require('webpack');
+// let ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-    entry: './src/index.js', // 入口文件
+    entry: './src/index.js', // 入口文件, 可以是数组， 同时打包多个没有饮用关系的文件, 还可以是对象
+    // entry: { // 多入口配置
+    //     index: './src/index.js',
+    //     a: './src/a.js'
+    // },
     output: {
-        filename: 'build.js',
+        filename: 'build.[hash:8].js', // 多入口 对应  多出口
+        // filename: '[name].[hash:8].js', // 多入口 对应  多出口
         path: path.resolve('./build')
     },
     devServer: {
         contentBase: '/build',
-        port: 3000,
+        port: 8081,
         compress: true, // 服务器压缩
-        open: true // 自动打开浏览器
+        open: true, // 自动打开浏览器
+        hot: true
     },
-    module: {},
-    plugins: [],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                    use: [
+                        { loader: 'style-loader' }, 
+                        { loader: 'css-loader' }
+                    ]
+            },
+            {
+                test: /\.less$/,use: [
+                    {loader: 'style-loader' }, 
+                    {loader: 'css-loader' },
+                    {loader: 'sass-loader' },
+                    {loader: 'node-sass' },
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // new ExtractTextWebpackPlugin({
+        //     filename: 'css/index.css'
+        // }),
+        new Webpack.HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            // filename: 'idx.html', // 指定build 输出多名字
+            template: './src/index.html',
+            title: 'webpack4.0',
+            hash: true,
+            // chunks: ['index'] // 插入idx.html 的时候，寻找叫 index 的js 文件
+            // minify: {
+            //     removeAttributeQuotes: true,
+            //     collapseWhitespace: true,
+            // }
+        }),
+        // new HtmlWebpackPlugin({
+        //     filename: 'a.html',
+        //     template: './src/a.html',
+        //     title: 'webpack4.0',
+        //     hash:true,
+        //     chunks: ['a']
+        //     // minify: {
+        //     //     removeAttributeQuotes: true,
+        //     //     collapseWhitespace: true,
+        //     // }
+        // })
+    ],
     mode: 'development',
     resolve: {},
 }
